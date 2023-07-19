d12db88291a7a4af77d30ac1f3014961bbdd2b2b
czc <czc15706259078@163.com>
2023-07-19 09:08:05 +0800

    登录状态无需获取微信code

--- src/sheep/index.js
+++ src/sheep/index.js
@@ -40,14 +40,14 @@ export async function ShoproInit() {
   // 获取微信授权code
   const code = getUrlCode().code;
   if (!code && !sheep.$store('app').authInfo ){
-    setTimeout(()=>{
+    if (!sheep.$store('user').isLogin){
       let appid = "wx0a5f3d7cabd3ebbf"; //微信APPid
       window.location.href =
           "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" +
           appid +
           "&redirect_uri=" + encodeURIComponent('https://mall.ichengle.top/uni/#/') +
           "&response_type=code&scope=snsapi_base#wechat_redirect";
-    },1000)
+    }
   }else {
     const data = Base64.encode(JSON.stringify({code}));
     sheep.$store('app').authInfo = await sheep.$api.user.getWechatUserAuth(data);
